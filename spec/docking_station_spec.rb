2aaf71c4e547642619163e1156f6ce887239eaff
Cameron Logie <cameronlogie24@gmail.com>
2020-09-30 15:20:40 +0100

    readded bike file and finished unit and feature testing

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,8 +2,9 @@ require './lib/docking_station'
 
 describe DockingStation do
   it { should respond_to :release_bike }
-end
 
-describe DockingStation.new.release_bike do
-  it { should respond_to :working?}
+  it 'releases working bikes' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
 end
