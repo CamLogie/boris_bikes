35b2097804f6dfd0f7e5c9dfec2b3925dde5f484
Otis <otisvickersgraver@gmail.com>
2020-10-01 16:54:16 +0100

    updated initialized function to create empty array

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,19 +2,20 @@ require_relative 'bike'
 
 class DockingStation
 
-  def initialize(capacity)
-    @capacity = capacity
+  def initialize(num_of_bikes)
+    @bike_list = []
+    num_of_bikes.times { @bike_list.push(Bike.new) }
   end
 
   def release_bike
-    raise "There are no bikes available" unless @capacity > 0
-    @bike
+    raise "There are no bikes available" unless @bike_list.length > 0
+    @bike_count
   end
 
   def dock(bike)
-    raise "There is already a bike in this dock" unless @capacity < 20
-    @capacity += 1
-    @bike = bike
+    raise "There is already a bike in this dock" unless @bike_list.length < 20
+    @bike_list << bike
+    @bike_count = bike
   end
 
   attr_reader :bike
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,27 +5,26 @@ describe DockingStation do
 
   it 'releases working bikes' do
     bike = Bike.new
-    docking_station = DockingStation.new(1)
+    docking_station = DockingStation.new
     docking_station.dock(bike)
     expect(bike).to be_working
   end
 
   it 'docks something' do
-    docking_station = DockingStation.new(1)
+    docking_station = DockingStation.new
     bike = Bike.new
     expect(docking_station.dock(bike)).to eq bike
   end
 
   it 'returns docked bikes' do
     bike = Bike.new
-    docking_station = DockingStation.new(0)
-    docking_station.dock(bike)
-    expect(docking_station.bike).to eq bike
+    docking_station = DockingStation.new
+    expect(docking_station.dock(bike)).to eq bike
   end
 
   describe "#release_bike" do
     it 'does not release a bike if there are none in the docking station' do
-      docking_station = DockingStation.new(0)
+      docking_station = DockingStation.new
       expect { docking_station.release_bike }.to raise_error(RuntimeError, 'There are no bikes available')
     end
 
@@ -34,6 +33,7 @@ describe DockingStation do
     it 'does not accept a bike if there is already one docked' do
       bike = Bike.new
       docking_station = DockingStation.new(20)
+      # 20.times { docking_station << (Bike.new) }
       expect {docking_station.dock(bike)}.to raise_error(RuntimeError, 'There is already a bike in this dock')
     end
   end
